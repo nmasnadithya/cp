e29ef9ea515cad061ade50d8265cbdb61f86f77e
Adithya Narasinghe <adithya@rccsonline.com>
2017-05-03 00:16:49 +0530

    Added the sample solution of USACO

---
+++ usaco/chapter 1/section 1.1/1-test.cpp
@@ -0,0 +1,18 @@
+/*
+ID: nmasnad1
+PROG: test
+LANG: C++11
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ofstream fout ("test.out");
+    ifstream fin ("test.in");
+    int a, b;
+    fin >> a >> b;
+    fout << a+b << endl;
+    return 0;
+}
